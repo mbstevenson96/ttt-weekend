38897d543a812d56f840ad3d6f05b3103e16d392
mstevenson <mbstevenson96@gmail.com>
2022-09-01 22:10:30 -0400

    create two cached element references

--- js/app.js
+++ js/app.js
@@ -3,10 +3,15 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let board, turn, winner
+// board = represent state of hte squares
+// turn = track whose turn it is
+// winner = represent if anyone has won yet or if there is a tie
 
 
 /*------------------------ Cached Element References ------------------------*/
+const squareEls = document.querySelector('.boardSquares')
+const messageEl = document.getElementById('message')
 
 
 
